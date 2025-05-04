5a89039f555ee4c949dea5d3b449665a496d1b50
Mrminino <cristianparadalopez@gmail.com>
2025-05-04 14:57:59 -0600

    crud invoices

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,20 @@
-export default function Page() {
-    return <p>Home</p>;
-  }
+
+import { lusitana } from '@/app/ui/fonts';
+
+ 
+export default async function Page() {
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+     
+      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense> */}
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
+}
